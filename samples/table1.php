52ec5388638867ed13b988b649ad6c73b981e4ce
Abdul Hanan <abdul.hanan@qummatic.com>
2011-07-18 07:57:14 -0700

    Edited samples/table1.php via GitHub

--- samples/table1.php
+++ samples/table1.php
@@ -10,11 +10,10 @@
  * @author      Abdul Hanan (http://hanan.qummatic.com)
  * @copyright   2010 bekasi-code.qummatic.com
  * @license     http://creativecommons.org/licenses/GPL/2.0/ CC-GNU GPL version 2 or later
- * @version     ver 0.1
+ * @version     ver 0.2
  * @link        http://bekasi-code.qummatic.com/html-object
  *
  */
-
 // Requires HTML Object Class Library
 require "../libs/HTML_Object.php";
 
@@ -28,19 +27,19 @@ $objHTMLTable = clone $objHTML->Table;
 $objHTMLTable->setClass('table-grid');
 
 // Registers table row with the header columns
-$objHTMLTable->row("row-head");
-$objHTMLTable->row->head("No", array('width'=>'20'));
-$objHTMLTable->row->head("City");
+$objHTMLTable->row("row-head")
+    ->head("No", array('width'=>'20'))
+	->head("City");
 $objHTMLTable->apply();
 
 // Looping proces for registering table row with the columns
 $i = 0;
 foreach ($city as $value) {
     $i++;
-    $objHTMLTable->row("row-$i");
-    $objHTMLTable->row->col($i.". ", array('align'=>'right'));
-    $objHTMLTable->row->col($value);
-    $objHTMLTable->apply();
+    $objHTMLTable->row("row-$i")
+        ->col($i . ". ", array('align'=>'right'))
+        ->col($value);
+	$objHTMLTable->apply();
 }
 
 // Fetches HTML Table and assigns to $table variable
@@ -53,11 +52,11 @@ $body = $objHTML->div($body, "content");
 
 // {{{ Scripts for creating HTML Document
 $objHTMLDocument = clone $objHTML->Document;
-$objHTMLDocument->head();
-$objHTMLDocument->head->title("HTML Object Sample - Table");
-$objHTMLDocument->head->cssURL("includes/tables.css");
-$objHTMLDocument->body();
-$objHTMLDocument->body->content($body);
+$objHTMLDocument->head()
+    ->title("HTML Object Sample - Table")
+	->cssURL("includes/tables.css");
+$objHTMLDocument->body()
+	->content($body);
 $objHTMLDocument->display();
 // }}}
 ?>
