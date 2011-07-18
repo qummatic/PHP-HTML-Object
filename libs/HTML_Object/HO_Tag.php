c43af1cf8278f2fc8591dbd1064c3ccfb1e38b7c
Abdul Hanan <abdul.hanan@qummatic.com>
2011-07-18 07:32:38 -0700

    Edited libs/HTML_Object/HO_Tag.php via GitHub

--- libs/HTML_Object/HO_Tag.php
+++ libs/HTML_Object/HO_Tag.php
@@ -15,7 +15,7 @@
  * @author      Abdul Hanan (http://hanan.qummatic.com) Original Author
  * @copyright   2011 bekasi-code.qummatic.com
  * @license     http://www.gnu.org/licenses/lgpl-3.0.txt LGPL Version 3
- * @version     ver 0.2-dev
+ * @version     ver 0.2
  * @link        http://bekasi-code.qummatic.com/html-object
  * 
  */
@@ -49,7 +49,7 @@
  * - onmouseup: HO_Tag::onmouseup(), Script to be run when mouse button is released on mouse events
  * 
  * @access private
- * @version class-ver 0.2-dev
+ * @version class-ver 0.2
  * @author Abdul Hanan (http://hanan.qummatic.com)
  */
 class HO_Tag extends HTML_Object
